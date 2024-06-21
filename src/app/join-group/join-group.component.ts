44d160f17fa33a3138e55be45e703e8b5f6944d1
Silarov <silarov2.0@gmail.com>
2024-06-21 11:35:32 +0200

    Navbar and Footer created, forgotten Component added

---
+++ src/app/join-group/join-group.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-group',
+  standalone: true,
+  imports: [],
+  templateUrl: './join-group.component.html',
+  styleUrl: './join-group.component.scss'
+})
+export class JoinGroupComponent {
+
+}
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -9,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  isDropdownOpen = false;
+  isMobileMenuOpen = false;
+
+  toggleMenu() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
